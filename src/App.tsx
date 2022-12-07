b9f466ae74bee33e30ddfe3ef10e8506e3ac5dc6
vovitolog <vovitolog@gmail.com>
2022-12-07 21:11:23 +0300

    feat: add main functional in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    const incHandler = () => {
+        setCount(count + 1);
+    }
+    const resetHandler = () => {
+        setCount(0);
+    }
+    return (
+        <div className="App">
+            <div>
+                {count}
+            </div>
+            <div>
+                <button
+                    onClick={incHandler}
+                    disabled={count === 5}
+                >INC
+                </button>
+                <button
+                    onClick={resetHandler}
+                    disabled={count !== 5}
+                >RESET
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
