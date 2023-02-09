e7ec17fcee143a66acef795972fe09d152023592
vovitolog <vovitolog@gmail.com>
2023-02-09 20:53:22 +0300

    refactor: unite variables in one state

--- src/App.tsx
+++ src/App.tsx
@@ -4,71 +4,104 @@ import {TestCounter} from "./components/TestCounter";
 import {Button} from "./components/Button/Button";
 import classes from "./components/Button/Button.module.css";
 import {InputValue} from "./components/InputValue/InputValue";
- import {CounterValue} from "./components/CounterValue/CounterValue";
+import {CounterValue} from "./components/CounterValue/CounterValue";
 
-function App() {
+function App(this: any) {
     const ERROR_MESSAGE = 'Incorrect value!';
     const VALID_MESSAGE = 'Enter values end press "set"';
 
-    const [maxValue, setMaxValue] = useState(localStorage.getItem('maxValue') || '0');
-    const [startValue, setStartValue] = useState(localStorage.getItem('startValue') || '0');
-    const [count, setCount] = useState(Number(startValue));
-    const [isSetButtonDisabled, setIsSetButtonDisabled] = useState<boolean>(true);
+   // const [maxValue, setMaxValue] = useState(localStorage.getItem('maxValue') || '0');
+  //  const [startValue, setStartValue] = useState(localStorage.getItem('startValue') || '0');
+    //const [count, setCount] = useState(Number(startValue));
+    const [isSetButtonDisabled, setIsSetButtonDisabled] = useState<boolean>(false);
     const [isMaximumCounterValueReached, setIsMaximumCounterValueReached] = useState<boolean>(false);
+    //const [isValueSet, setIsValueSet] = useState(true);
+    const [error, setError] = useState(false);
 
-    const onChangeValue = (value: string) => {
-        //  setMaxValue(value);
+    const initialState = {
+        startValue: localStorage.getItem('startValue') || '0',
+        maxValue: localStorage.getItem('maxValue') || '0',
+        counterValue: Number(localStorage.getItem('startValue') || '0'),
+        error: false,
     }
 
+    const [state, setState] = useState(initialState);
+    console.log(state);
+
+    // проверка корректности введённых значений
+    // const checkIsValuesCorrect = () => {
+    //     if (Number(startValue) >= Number(maxValue)) return false;
+    //     if (Number(startValue) < 0) return false;
+    //     if (Number(maxValue) < 1) return false;
+    //     if (Number(maxValue) === Number(startValue)) return false;
+    //     return true;
+    // }
+
     const onChangeMaxValue = (value: string) => {
-        setIsSetButtonDisabled(false);
-        setMaxValue(value);
-        // console.log(value + '  test')
-        // console.log(maxValue + ' Max');
+        // setError(!checkIsValuesCorrect());
+        // if (error) {
+          //  setIsSetButtonDisabled(true)
+        // }
+        // ;
+        // if (!error) {
+           setIsSetButtonDisabled(false)
+        // }
+        // ;
+
+
+        //    setMaxValue(value);
+
+        setState({...state, maxValue: value})
     }
 
     const onChangeStartValue = (value: string) => {
-        setIsSetButtonDisabled(false);
-        setStartValue(value);
         // console.log(startValue + ' Start');
+        // setIsValueSet(false);
+        // if (error) {
+        //     setIsSetButtonDisabled(true)
+        // }
+        // ;
+        // if (!error) {
+            setIsSetButtonDisabled(false)
+        // }
+        // ;
+        setState({...state, startValue: value})
     }
 
     const incHandler = () => {
-       // checkMaximumValueReached();
-        setCount(count + 1);
+        console.log(state.counterValue);
+        //setCount(count + 1);
+        setState({...state, counterValue: state.counterValue + 1});
     }
     const resetHandler = () => {
-        setCount(Number(startValue));
+        //  setCount(Number(startValue));
+        setState({...state, counterValue: Number(state.startValue)})
     }
 
     const setHandler = () => {
-        // console.log('set')
-        setCount(Number(startValue));
-        localStorage.setItem('startValue', startValue);
-        localStorage.setItem('maxValue', maxValue);
+       // setCount(Number(state.startValue));
+        setState({...state, counterValue: Number(state.startValue)})
+        localStorage.setItem('startValue', state.startValue);
+        localStorage.setItem('maxValue', state.maxValue);
         setIsSetButtonDisabled(true);
+        // setIsValueSet(true);
     }
 
-    // const checkMaximumValueReached = () => {
-    //     if (count === Number(maxValue)) {
-    //         setIsMaximumCounterValueReached(true);
-    //         console.log('ttttttttttttt')
-    //     }
-    // }
-
-    useEffect(()=>{
-        if (count === Number(maxValue)) {
+    // Смена класса номера при вводе
+    useEffect(() => {
+        if (state.counterValue === Number(state.maxValue)) {
             setIsMaximumCounterValueReached(true);
-            console.log('ttttttttttttt')
-        } else  setIsMaximumCounterValueReached(false);
-    }, [count])
+        } else setIsMaximumCounterValueReached(false);
+    }, [state.counterValue])
+
 
     return (
         <div className="App">
             <div className={'container-wrapper'}>
                 <div className={'input-wrapper'}>
-                    <InputValue description={'max value: '} currentValue={maxValue} onChangeValue={onChangeMaxValue}/>
-                    <InputValue description={'start value: '} currentValue={startValue}
+                    <InputValue description={'max value: '} currentValue={state.maxValue}
+                                onChangeValue={onChangeMaxValue}/>
+                    <InputValue description={'start value: '} currentValue={state.startValue}
                                 onChangeValue={onChangeStartValue}/>
                 </div>
                 <div className={'button-wrapper'}>
@@ -77,9 +110,16 @@ function App() {
             </div>
             <div className={'container-wrapper'}>
                 {/*<div className={'number'}>{count}</div>*/}
-                <CounterValue value={count.toString(10)} maximumReached={isMaximumCounterValueReached}/>
+                {//isValueSet ?
+                    <CounterValue value={state.counterValue.toString(10)}
+                                  maximumReached={isMaximumCounterValueReached}/>
+
+                    // <CounterValue value={VALID_MESSAGE} maximumReached={isMaximumCounterValueReached}/>
+                }
+
                 <div className={'button-wrapper'}>
-                    <Button name={'inc'} handler={incHandler} disabled={count === Number(maxValue)}></Button>
+                    <Button name={'inc'} handler={incHandler}
+                            disabled={state.counterValue === Number(state.maxValue)}></Button>
                     <Button name={'reset'} handler={resetHandler} disabled={false}></Button>
                 </div>
             </div>
